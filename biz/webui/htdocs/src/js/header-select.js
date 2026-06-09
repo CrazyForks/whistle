5d843cc2260aa3fd2887d12da475f8d7455cdd41
avwo <avwu@qq.com>
2026-06-09 17:35:10 +0800

    feat: default headers

--- biz/webui/htdocs/src/js/header-select.js
+++ biz/webui/htdocs/src/js/header-select.js
@@ -8,27 +8,44 @@ var HeaderSelect = React.createClass({
   updateOptions: function() {
     var props = this.props;
     var session = props.session;
-    var options = [];
+    var options = [
+      'host',
+      'user-agent',
+      'referer',
+      'content-type',
+      'origin',
+      'access-control-request-method',
+      'access-control-request-headers',
+      'access-control-allow-origin',
+      'access-control-allow-methods',
+      'access-control-allow-headers',
+      'access-control-expose-headers',
+      'access-control-allow-credentials',
+      'access-control-max-age',
+      'x-forwarded-for',
+      'x-real-ip'
+    ];
+    var addOption = function(name) {
+      if (options.indexOf(name) === -1) {
+        options.push(name);
+      }
+    };
     var hasChanged;
     if (session) {
       var headers = session.req.headers;
       if (this._reqHeaders !== headers) {
         this._reqHeaders = headers;
         hasChanged = true;
-        Object.keys(headers).forEach(function(name) {
-          options.push(name);
-        });
+        Object.keys(headers).forEach(addOption);
       }
       headers = session.res.headers;
       if (this._resHeaders !== headers) {
         this._resHeaders = headers;
         hasChanged = true;
-        headers && Object.keys(headers).forEach(function(name) {
-          options.push(name);
-        });
+        headers && Object.keys(headers).forEach(addOption);
       }
     }
-    if (hasChanged) {
+    if (hasChanged || !this.state.options.length) {
       this.setState({ options: options });
     }
   },
